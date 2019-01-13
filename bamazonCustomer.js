92ebe4f5627189286b0e56f28b2c594afb2a56f6
hoang <hoangcao@mail.weber.edu>
2019-01-12 18:35:45 -0700

    added databases

---
+++ bamazonCustomer.js
@@ -0,0 +1,33 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "admin",
+    database: "bamazon"
+  });
+
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+
+    var queryString = "SELECT * FROM products";
+        connection.query(queryString, function(err, result) {
+          if (err) throw err;
+          console.log(result);
+          inquirer.prompt({
+            type: 'input',
+            name: 'ID question',
+            message: 'Which Product ID do you want to buy?'
+          }),function(response)
+          {
+               
+          };
+        });
+  });
+
+  module.exports = connection;
---
+++ schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+  item_id VARCHAR(30) NOT NULL,
+  product_name VARCHAR(30) NOT NULL,
+  department_name VARCHAR(30) NOT NULL,
+  price INTEGER(10)  NOT NULL,
+  stock_quantity INTEGER(10) ,
+  PRIMARY KEY (item_id)
+);
